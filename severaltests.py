95a842eac9ff6964d8e025dfef8814e425ff19e1
Jorge Amorim <jfbam@iscte-iul.pt>
2020-11-22 02:33:45 +0000

    Day 3 - Beginner - Control Flow and Logical Operators

---
+++ severaltests.py
@@ -0,0 +1,85 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+fname = 0
+sname = 0
+
+name1 = name1.lower()
+name2 = name2.lower()
+
+fname += name1.count('t')
+fname += name2.count('t')
+fname += name1.count('r')
+fname += name2.count('r')
+fname += name1.count('u')
+fname += name2.count('u')
+fname += name1.count('e')
+fname += name2.count('e')
+
+sname += name1.count('l')
+sname += name2.count('l')
+sname += name1.count('o')
+sname += name2.count('o')
+sname += name1.count('v')
+sname += name2.count('v')
+sname += name1.count('e')
+sname += name2.count('e')
+
+total = fname * 10 + sname
+
+if total < 10 or total > 90:
+  print(f"Your score is {total}, you go together like coke and mentos.")
+elif total >= 40 and total <= 50:
+  print(f"Your score is {total}, you are alright together.")
+else:
+  print(f"Your score is {total}.")
+
+
+#Write your code above this line 👆
+# 🚨 Do NOT modify the code below this line 👇
+
+
+with open('testing_copy.py', 'w') as file:
+  file.write('def test_func():\n')
+  with open('main.py', 'r') as original:
+    f2 = original.readlines()[0:45]
+    for x in f2:
+      file.write("    " + x)
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import os
+
+class MyTest(unittest.TestCase):
+  def run_test(self, given_answer, expected_print):
+    with patch('builtins.input', side_effect=given_answer), patch('sys.stdout', new=StringIO()) as fake_out:
+      testing_copy.test_func()
+      self.assertEqual(fake_out.getvalue(), expected_print)
+
+  def test_1(self):
+    self.run_test(given_answer=['David Beckham', 'Victoria Beckham'], expected_print='Welcome to the Love Calculator!\nYour score is 45, you are alright together.\n')
+
+  def test_2(self):
+    self.run_test(given_answer=['Han Solo', 'Princess Leia Organa'], expected_print='Welcome to the Love Calculator!\nYour score is 47, you are alright together.\n')
+
+  def test_3(self):
+    self.run_test(given_answer=['Pierre Curie', 'Marie Curie'], expected_print='Welcome to the Love Calculator!\nYour score is 125, you go together like coke and mentos.\n')
+
+  def test_4(self):
+    self.run_test(given_answer=['Mark Antony', 'Cleopatra'], expected_print='Welcome to the Love Calculator!\nYour score is 54.\n')
+
+
+print('\n\n\n.\n.\n.')
+print('Checking if your print statements match the instructions. \nFor "Mario" and "Princess Peach" your program should print this line *exactly*:\n')
+print('Your score is 43, you are alright together.\n')
+print('\nRunning some tests on your code with different name combinations:')
+print('.\n.\n.')
+unittest.main(verbosity=1, exit=False)
+
+os.remove('testing_copy.py')
