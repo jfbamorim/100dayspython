1dcca0ad59dbfcfd06110ac7f1c3b95558da3be7
Jorge Amorim <jfbam@iscte-iul.pt>
2020-11-28 13:42:43 +0000

    Day 8 - Beginner - Caesar Cipher - Versão Final

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,25 +1,63 @@
+import caesar_cypher_art
 #####################################################################
 # Final Project - Caesar Cipher
 #####################################################################
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(caesar_cypher_art.logo)
 
 
-def encrypt(text, shift):
-    encrypted_text = ""
-    idx_loop = 0
-    for char in text:
-        idx_alphabet = alphabet.index(char.lower())
-        idx_total = idx_alphabet + shift
-        if idx_total > 25:
-            idx_total %= 26
-        encrypted_text += alphabet[idx_total]
-        idx_loop += 1
-    print(f"The encoded text is {encrypted_text}")
+def caesar(text, shift, direction):
+    text_output = ""
+    for letter in text:
+        if letter.isalpha():
+            idx_alphabet = alphabet.index(letter.lower())
+            if direction.lower() == 'encode':
+                idx_total = idx_alphabet + shift
+                if idx_total > 25:
+                    idx_total %= 26
+                text_output += alphabet[idx_total]
+            elif direction.lower() == 'decode':
+                idx_total = idx_alphabet - shift
+                text_output += alphabet[idx_total]
+        else:
+            text_output += letter
+    print(f"The {direction}d text is {text_output}")
 
 
-encrypt(text=text, shift=shift)
+answer = "yes"
+while answer == 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text=text, shift=shift, direction=direction)
+    answer = input("Type yes if you want to go again. Otherwise type no.").lower()
+print("Good bye.")
+
+
+# 1st implementation
+#def encrypt(text, shift):
+#    encrypted_text = ""
+#    for char in text:
+#        idx_alphabet = alphabet.index(char.lower())
+#        idx_total = idx_alphabet + shift
+#        if idx_total > 25:
+#            idx_total %= 26
+#        encrypted_text += alphabet[idx_total]
+#    print(f"The encoded text is {encrypted_text}")
+
+
+#def decrypt(text, shift):
+#    decrypted_text = ""
+#    for char in text:
+#        idx_alphabet = alphabet.index(char.lower())
+#        idx_total = idx_alphabet - shift
+#        decrypted_text += alphabet[idx_total]
+#    print(f"The decoded text is {decrypted_text}")
+
+
+#if direction.lower() == 'encode':
+#    encrypt(text=text, shift=shift)
+#elif direction.lower() == 'decode':
+#    decrypt(text=text, shift=shift)
