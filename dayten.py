3ab98b0b06746cb938470eace947be32d992f258
Jorge Amorim <jfbam@iscte-iul.pt>
2020-11-29 03:05:33 +0000

    Day 10 - Beginner - Functions with Output

---
+++ dayten.py
@@ -0,0 +1,51 @@
+####################################################################
+# Day 10 - Functions with output
+####################################################################
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_name} {formatted_l_name}"
+
+
+title_name = format_name("jorge", "amorim")
+print(title_name)
+
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_month = month_days[month-1]
+    if is_leap(year):
+        days_month += 1
+    return days_month
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+
+
+
+
+
+
+
+
+
+
+
