b15891f2204f842bf6201bf4f18a7b39fcc00101
Jorge Amorim <jfbam@iscte-iul.pt>
2020-12-01 00:38:10 +0000

    Day 10 - Beginner - Calculator Project

--- calculator.py
+++ calculator.py
@@ -30,13 +30,31 @@ operations = {
     "/": divide
 }
 
-print(logo)
-num1 = int(input("What's the first number? "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number? "))
 
-calc_function = operations[operation_symbol]
-answer = calc_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+def calculator():
+    repeat = 'y'
+    num1 = int(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    while repeat == 'y':
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What's the next number? "))
+
+        calc_function = operations[operation_symbol]
+        answer = calc_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        repeat = input(f"Type y to continue calculating with {answer}, or n to start a new calculation: ").lower()
+        if repeat == 'y':
+            num1 = answer
+        else:
+            calculator()
+
+# Body
+print(logo)
+calculator()
+
+#operation_symbol = input("Pick another operation: ")
+#num3 = int(input("What's the next number? "))
+#calc_function = operations[operation_symbol]
+#second_answer = calc_function(calc_function(num1, num2), num3)
+#print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
