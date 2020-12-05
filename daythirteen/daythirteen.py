d90cd080ccb0e4fc3ba6e38f8ff348d463370d91
Jorge Amorim <jfbam@iscte-iul.pt>
2020-12-05 03:11:32 +0000

    Day 13 - Beginner - Debugging and fixing errors in code

--- daythirteen/daythirteen.py
+++ daythirteen/daythirteen.py
@@ -1,5 +1,5 @@
 ################################################################
-# Day 13 - Beginner - Debugging and fixing errors in code
+# Day 13 - Beginner - Debugging and fixing errors in code 
 ################################################################
 ############DEBUGGING#####################
 
@@ -53,12 +53,13 @@ word_per_page == int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    # b_list.append(new_item) - cannot be here
+    print(b_list)
 
-# mutate([1,2,3,5,8,13])
+mutate([1,2,3,5,8,13])
