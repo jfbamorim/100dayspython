f8c43b3515350539b5ef326ef639776125fdf1b3
Jorge Amorim <jfbam@iscte-iul.pt>
2020-12-05 03:15:24 +0000

    Day 1 - Beginner - Working w/ Variables in Python to manage data

--- dayone/dayone.py
+++ dayone/dayone.py
@@ -7,4 +7,4 @@ pet_name = (input("What's your pet's name?\n"))
 #4. Combine the name of their city and pet and show them their band name.
 print("Your band name could be " + city + " " + pet_name)
 #5. Make sure the input cursor shows on a new line, see the example at:
-#   https://band-name-generator-end.appbrewery.repl.run/ 
+#   https://band-name-generator-end.appbrewery.repl.run/
