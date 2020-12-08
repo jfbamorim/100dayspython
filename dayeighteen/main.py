7f33fb1e948936a1cd1f41ff095a9710a452b75c
Jorge Amorim <jfbam@iscte-iul.pt>
2020-12-08 17:43:57 +0000

    Day 18 - Intermediate - Graphical User Interface (GUI)

--- dayeighteen/main.py
+++ dayeighteen/main.py
@@ -1,8 +1,12 @@
 from turtle import Turtle, Screen
+import random
 # Several ways to import modules
 # from turtle import *
 # import turtle as t
 
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+movements = [0, 90, 180, 270]
 
 def move_to_right():
     timmy_the_turtle.right(90)
@@ -12,6 +16,7 @@ def move_to_right():
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
+
 # advance turtle forward
 # timmy_the_turtle.forward(100)
 # turning turtle to south
@@ -23,14 +28,34 @@ timmy_the_turtle.color("red")
 # timmy_the_turtle.forward(100)
 
 # Draw a dashed line - https://opentechschool.github.io/python-beginners/en/loops.html
-for _ in range(15):
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.pendown()
+# for _ in range(15):
+#    timmy_the_turtle.forward(10)
+#    timmy_the_turtle.penup()
+#    timmy_the_turtle.forward(10)
+#    timmy_the_turtle.pendown()
 
 # timmy_the_turtle.right(90)
 # timmy_the_turtle.forward(100)
+
+# Draw different shapes
+# for i in range(3, 11):
+#   j = i
+#    #color = random.choices(range(255), k=3)
+    #timmy_the_turtle.color(color)
+#    while j > 0:
+#        angle = 360 / i
+#        timmy_the_turtle.forward(100)
+#        timmy_the_turtle.right(angle)
+#        j -= 1
+
+# Generate a random walk
+timmy_the_turtle.pensize(10)
+for _ in range(200):
+    timmy_the_turtle.speed("fastest")
+    timmy_the_turtle.color(random.choice(colours))
+    timmy_the_turtle.forward(30)
+    timmy_the_turtle.setheading(random.choice(movements))
+
 
 screen = Screen()
 screen.exitonclick()
